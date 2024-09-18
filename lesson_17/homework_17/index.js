0c79f48e596802a42a4ccec643b9d90a8750195c
Lina Göbel <galina283@freenet.de>
2024-09-18 18:03:38 +0200

    add lesson

--- lesson_17/homework_17/index.js
+++ lesson_17/homework_17/index.js
@@ -5,16 +5,14 @@
 // Дополнительная задача: Создайте кнопку "Filter", при нажатии на которую на странице будут
 //  отображаться только блоки с ценами > 450
 
+const container = document.querySelector("#price-container");
+const filterButton = document.querySelector("#filter");
 
 const  prices = [100, 400, 900, 300, 500];
 
-const container = document.querySelector("#price-container");
-
 const listPrices = (createPriceList)=>{
-    container.innerHTML = "";
     createPriceList.forEach((price)=>{
         const priceBlock = document.createElement("div");
-        priceBlock.classList.add("price-block");
         priceBlock.textContent =  `Price: ${price}`;
         container.append(priceBlock);
     });
@@ -23,11 +21,11 @@ const listPrices = (createPriceList)=>{
 listPrices(prices);
 
 const filterPrices = ()=>{
+    container.innerHTML = ``;
     const  filteredPrices = prices.filter((price)=> price > 450);
     listPrices(filteredPrices);
 };
 
-const filterButton = document.querySelector("#filter");
 filterButton.addEventListener("click", filterPrices);
 
 
